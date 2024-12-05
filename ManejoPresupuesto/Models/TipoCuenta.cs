909a28b3500642850d27b96dcba0ef8945827dee
Heyner Leiva <heynerleiva96@gmail.com>
2024-12-05 09:18:34 -0600

    Agregar archivos de proyecto.

---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,48 @@
+﻿using ManejoPresupuesto.Validaciones;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta //: IValidatableObject
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        //[StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")]
+        //[Display(Name = "Nombre del tipo cuenta")]
+
+        // Validacion Personalizada
+        [PrimeraLetraMayuscula]
+        [Remote(action: "VerificarExisteTipoCuenta", controller: "TiposCuentas", AdditionalFields = nameof(Id))]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+        /*public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Nombre != null && Nombre.Length > 0)
+            {
+                var primeraLetra = Nombre[0].ToString();
+                if(primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayúscula",
+                        new[] {nameof(Nombre) });
+                }
+            }
+        }*/
+
+        /* Pruebas de otras validaciones por defectos */
+        /*[Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El campo debe ser un correo electrónico válido")]
+        public string Email { get; set; }
+        [Range(minimum:18, maximum:130, ErrorMessage = "El valor debe de estar entre {1} y {2}")]
+        public int Edad { get; set; }
+        [Url(ErrorMessage = "El campo debe ser una URL válida")]
+        public string URL { get; set; }
+        [CreditCard(ErrorMessage = "La tarjeta de crédito no es válida")]
+        [Display(Name = "Tarjeta de Crédito")]
+        public string TarjetaDeCredito { get; set; } */
+
+    }
+}
---
+++ ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
@@ -0,0 +1,90 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioTiposCuentas
+    {
+        Task Actualizar(TipoCuenta tipoCuenta);
+        Task Borrar(int id);
+        Task Crear(TipoCuenta tipoCuenta);
+        Task<bool> Existe(string nombre, int usuarioId, int id = 0);
+        Task<IEnumerable<TipoCuenta>> Obtener(int usuarioId);
+        Task<TipoCuenta> ObtenerPorId(int id, int usuarioId);
+        Task Ordenar(IEnumerable<TipoCuenta> tipoCuentasOrdenados);
+    }
+    public class RepositorioTiposCuentas: IRepositorioTiposCuentas
+    {
+        private readonly string connectionStrings;
+        public RepositorioTiposCuentas(IConfiguration configuration)
+        {
+            connectionStrings = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(TipoCuenta tipoCuenta)
+        {
+            using var connection = new SqlConnection(connectionStrings);
+            var id = await connection.QuerySingleAsync<int>("TipoCuentas_Insertar",
+                    new { usuarioId = tipoCuenta.UsuarioId, nombre = tipoCuenta.Nombre },
+                    commandType: System.Data.CommandType.StoredProcedure
+                );
+
+            tipoCuenta.Id = id;
+        }
+
+        public async Task<bool> Existe(string nombre, int usuarioId, int id = 0)
+        {
+            using var connection = new SqlConnection(connectionStrings);
+            var existe = await connection.QueryFirstOrDefaultAsync<int>(
+                @"SELECT 1 
+                FROM TiposCuentas WHERE Nombre = @Nombre AND UsuarioId = @UsuarioId AND Id <> @id;",
+                new { nombre, usuarioId, id }
+               );
+            return existe == 1;
+        }
+
+        public async Task<IEnumerable<TipoCuenta>> Obtener(int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionStrings);
+            return await connection.QueryAsync<TipoCuenta>(
+                @"SELECT Id, Nombre, Orden FROM TiposCuentas WHERE UsuarioId = @UsuarioId
+                ORDER BY Orden",
+                new { usuarioId }
+            );
+        }
+
+        public async Task Actualizar(TipoCuenta tipoCuenta)
+        {
+            using var connection = new SqlConnection(connectionStrings);
+            await connection.ExecuteAsync(@"UPDATE TiposCuentas
+                SET Nombre = @Nombre 
+                WHERE id = @id;",
+            tipoCuenta);
+
+        }
+
+        public async Task<TipoCuenta> ObtenerPorId(int id, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionStrings);
+            return await connection.QueryFirstOrDefaultAsync<TipoCuenta>(@"SELECT Id, Nombre, Orden
+                FROM TiposCuentas
+                WHERE Id = @Id AND UsuarioId = @UsuarioId;",
+            new { id, usuarioId });
+        }
+
+        public async Task Borrar(int id)
+        {
+            using var connection = new SqlConnection(connectionStrings);
+            await connection.ExecuteAsync(@"DELETE TiposCuentas WHERE Id = @Id;", new { id });
+        }
+
+        public async Task Ordenar(IEnumerable<TipoCuenta> tipoCuentasOrdenados)
+        {
+            var query = "UPDATE TiposCuentas SET Orden = @Orden WHERE Id = @Id;";
+            using var connection = new SqlConnection(connectionStrings);
+            await connection.ExecuteAsync(query, tipoCuentasOrdenados);
+        }
+    }
+}
+
